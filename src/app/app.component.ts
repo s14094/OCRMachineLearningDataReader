b09894a1cea042a652658932e4b6e23159b157e0
Paweł Łakomiec <pawel.lakomiec@dahliamatic.pl>
2020-02-21 17:51:37 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from './app.service';
+import {MatTableDataSource} from '@angular/material';
+import {AppModel} from './app.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'OcrWebDisplay';
+  listModel: AppModel[] = [];
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = ['fieldValue', 'lang', 'lineBaseline', 'lineLeft', 'lineRight', 'lineBottom', 'lineTop', 'parAlign',
+    'parStartIndent', 'parLineSpacing', 'cellWidth', 'cellHeight', 'blockType', 'blockName', 'blockLeft', 'blockRight', 'blockTop',
+    'blockBottom', 'blockCenterCoords', 'blockWidth', 'blockHeight', 'pageWidth', 'pageHeight', 'pageResolution'];
+
+
+  constructor(
+    private appService: AppService
+  ) {
+  }
+
+
+  ngOnInit() {
+    this.appService.getData().subscribe(result => {
+      this.listModel = result;
+      console.log(this.listModel);
+      this.dataSource = new MatTableDataSource(this.listModel);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,86 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {
+  MatBadgeModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatTooltipModule,
+} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatChipsModule} from '@angular/material/chips';
+import {AppComponent} from './app.component';
+import {AppService} from './app.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatMenuModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+    MatBadgeModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+    MatStepperModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
