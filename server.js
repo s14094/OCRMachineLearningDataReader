d8e395db240c619472aa1fa71d39522490959e20
Paweł Łakomiec <pawel.lakomiec@dahliamatic.pl>
2020-02-24 11:19:49 +0100

    no message

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/OcrWebDisplay'));
 
 app.listen(process.env.PORT || 8080);
 
 //Path Location Strategy
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/OcrWebDisplay/index.html'));
 })
