be5c2d6ea042b8421ff5d31a653ad7d6380e0786
Paweł Łakomiec <pawel.lakomiec@dahliamatic.pl>
2020-02-26 10:28:12 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,8 @@ export class AppComponent implements OnInit {
   title = 'OcrWebDisplay';
   listModel: AppModel[] = [];
   dataSource = new MatTableDataSource();
-  displayedColumns: string[] = ['fieldValue', 'blockId', 'lang', 'lineBaseline', 'lineCenterX', 'lineCenterY',  'lineLeft', 'lineRight', 'lineBottom', 'lineTop',
+  displayedColumns: string[] = ['fieldValue', 'blockId', 'lang', 'lineBaseline',  'lineLeft', 'lineRight', 'lineBottom', 'lineTop',
+    'lineCenterX', 'lineCenterY',
     'parAlign', 'parStartIndent', 'parLineSpacing', 'cellWidth', 'cellHeight', 'blockType', 'blockName', 'blockLeft', 'blockRight',
     'blockTop', 'blockBottom', 'blockWidth', 'blockHeight', 'pageWidth', 'pageHeight', 'pageResolution',
     'probabilityInvoiceNumber'];
@@ -23,7 +24,6 @@ export class AppComponent implements OnInit {
     private appService: AppService
   ) {
   }
-
 
   ngOnInit() {
     this.appService.getData().subscribe(result => {
@@ -49,4 +49,10 @@ export class AppComponent implements OnInit {
       this.dataSource = new MatTableDataSource(this.listModel);
     });
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
